c1b3e23e0b71f0f317a949f8fcf1354fe445dc08
vinicius010neto <74117559+vinicius010neto@users.noreply.github.com>
2025-11-17 08:57:23 -0300

    feat: habilita execuÃ§Ã£o assÃ­ncrona no LoggingConfig

--- src/main/java/br/gov/se/setc/config/LoggingConfig.java
+++ src/main/java/br/gov/se/setc/config/LoggingConfig.java
@@ -1,10 +1,10 @@
 package br.gov.se.setc.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-/**
- * Configuração para habilitar AOP e logging
- */
+import org.springframework.scheduling.annotation.EnableAsync;
+
 @Configuration
 @EnableAspectJAutoProxy
+@EnableAsync
 public class LoggingConfig {
 }
