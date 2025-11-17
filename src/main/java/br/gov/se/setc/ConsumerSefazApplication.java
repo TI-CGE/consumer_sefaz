390c76273ea21eccee04d34b9b77d8e5eb5fc7b5
vinicius010neto <74117559+vinicius010neto@users.noreply.github.com>
2025-11-17 11:07:35 -0300

    fix: adiciona mÃ©todo configure na classe principal para suporte WAR

--- src/main/java/br/gov/se/setc/ConsumerSefazApplication.java
+++ src/main/java/br/gov/se/setc/ConsumerSefazApplication.java
@@ -2,12 +2,19 @@ package br.gov.se.setc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class ConsumerSefazApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ConsumerSefazApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerSefazApplication.class, args);
 	}
