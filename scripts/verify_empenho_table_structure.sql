e01f09de22dd6a1635853e9aedd21646df278b5d
vinicius010neto <74117559+vinicius010neto@users.noreply.github.com>
2026-02-09 16:06:02 -0300

    chore(scripts): atualiza verify_empenho_table_structure

--- scripts/verify_empenho_table_structure.sql
+++ scripts/verify_empenho_table_structure.sql
@@ -41,3 +41,6 @@ BEGIN
         RAISE NOTICE 'Tabela consumer_sefaz.empenho não encontrada. Nenhuma alteração aplicada.';
     END IF;
 END$$;
+
+CREATE UNIQUE INDEX IF NOT EXISTS uq_empenho
+ON consumer_sefaz.empenho (dt_ano_exercicio_ctb, cd_unidade_gestora, cd_gestao, sq_empenho);
